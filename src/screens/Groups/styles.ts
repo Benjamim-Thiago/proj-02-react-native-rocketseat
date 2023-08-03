22e019f842ace0da278a49d4240c3ec7a516dfe2
Benjamim.Teixeira <benjamim.teixeira@portoseguro.com.br>
2023-08-03 07:17:11 -0300

    make group screen

---
+++ src/screens/Groups/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components/native';
+import { CaretLeft } from 'phosphor-react-native';
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_600};
+  padding: 24px;
+`;
+
+export const BackButton = styled.TouchableOpacity`
+  flex: 1;
+`;
+
+export const BackIcon = styled(CaretLeft).attrs(({ theme }) => ({
+  size: 36,
+  color: theme.COLORS.WHITE
+}))``;
