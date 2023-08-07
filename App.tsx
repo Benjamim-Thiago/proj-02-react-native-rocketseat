3cbb8bc09d305e4bb529ed139cd8b85a66d7763d
Benjamim.Teixeira <benjamim.teixeira@portoseguro.com.br>
2023-08-07 08:29:29 -0300

    create all screens

--- App.tsx
+++ App.tsx
@@ -3,8 +3,8 @@ import {ThemeProvider } from 'styled-components';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import theme from './src/theme';
-import { Groups } from '@screens/Groups';
 import { Loading } from '@components/Loading';
+import { Groups } from '@screens/Groups';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
